719b8e634dd2f49b592b88a03ec5a4ffcd9d8a9d
Rodolfo D. Arce Sannemann <rodolfo.arce@wazuh.com>
2025-03-10 09:40:44 -0300

    Code re-write to handle arguments

---
+++ wazuh_docker_query/docker_query.py
@@ -0,0 +1,111 @@
+import json
+import subprocess
+import argparse
+import logging
+from pathlib import Path
+from socket import AF_UNIX, SOCK_DGRAM, socket
+
+location = 'agent-queue'
+WAZUH_SOCKET = f'/var/ossec/queue/sockets/queue'
+
+## Logging options
+# https://docs.python.org/3/howto/logging-cookbook.html#logging-cookbook
+# create file handler which logs even debug messages
+logger = logging.getLogger("docker-report")
+logger.setLevel(logging.INFO)
+fh = logging.StreamHandler()
+fh.setLevel(logging.INFO)
+fh_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+fh.setFormatter(fh_formatter)
+logger.addHandler(fh)
+
+def getContainers(docker_socket_file = '/var/run/docker.sock', docker_socket_query = 'http://localhost/containers/json'):
+    try:
+        containers = subprocess.Popen(['/usr/bin/curl', '--unix-socket', docker_socket_file , docker_socket_query] ,stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+        output, errors = containers.communicate()
+        r = json.loads(output)
+        #print(str(r))
+    except Exception as error:
+        print('General error: {0}',error)
+        exit(1)
+        
+    return (containers)
+
+def postContainersToSocket(containers):
+    for container in containers:
+        if container["State"] == 'running':
+            msg = { 'service': 'docker', 'docker': container }
+            #print(json.dumps(container))
+            string = '1:{0}->docker:{1}'.format(location, json.dumps(msg))
+            print(string)
+            try:
+                sock = socket(AF_UNIX, SOCK_DGRAM)
+                sock.connect(WAZUH_SOCKET)
+                sock.send(string.encode())
+                sock.close()
+            except FileNotFoundError:
+                print('# Error: Unable to open socket connection at %s' % WAZUH_SOCKET)
+                exit(2)
+
+def postContainerstoFile(containers, output_file):
+    logger.debug("Saving containers information to : %s" % output_file)
+    try:
+        f = open(output_file, 'a+')
+        for container in containers:
+            if container["State"] == 'running':
+                msg = { 'service': 'docker', 'docker': container }
+                f.write(msg)
+    except IOError:
+        logger.error("Error opening output file")
+        exit(3)
+
+if __name__ == "__main__":
+    # Read parameters using argparse
+    ## Initialize parser
+    parser = argparse.ArgumentParser()
+    ## Adding optional argument
+    parser.add_argument("-c", "--containers", help = "Obtain running container list", action="store_true")
+    parser.add_argument("-o", "--output", help = "Log output to file")
+    parser.add_argument("-D", "--debug", help = "Enable debug", action="store_true")
+    #parser.add_argument("-l", "--local", help = "Use local file to store events, requires: -d DIR|FILE", action="store")
+    ## Read arguments from command line
+    args = parser.parse_args()
+    
+    ## Log to file or stdout
+    # https://docs.python.org/3/howto/logging-cookbook.html#logging-cookbook
+    # create file handler which logs even debug messages
+    logger = logging.getLogger("docker-output")
+    
+    if args.debug:
+        logger.setLevel(logging.DEBUG)
+    else:
+        logger.setLevel(logging.INFO)
+    # If file is set, everything goes there
+    if args.output:
+        # create console handler with a higher log level
+        fh = logging.FileHandler(args.output)
+        # Define log level
+        if args.debug == True:
+            fh.setLevel(logging.DEBUG)
+            fh_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        else:
+            fh.setLevel(logging.INFO)
+            fh_formatter = logging.Formatter('%(message)s')
+        
+        fh.setFormatter(fh_formatter)
+        # add the handlers to the logger
+        logger.addHandler(fh)
+    else:
+        # create console handler with a higher log level
+        fh = logging.StreamHandler()
+        # Define log level
+        if args.debug == True:
+            fh.setLevel(logging.DEBUG)
+            fh_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        else:
+            fh.setLevel(logging.INFO)
+            fh_formatter = logging.Formatter('%(message)s')
+        
+        fh.setFormatter(fh_formatter)
+        # add the handlers to the logger
+        logger.addHandler(fh)
