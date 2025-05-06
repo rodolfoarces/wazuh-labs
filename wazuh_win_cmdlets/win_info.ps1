baddcf098e7ec00f6467cdf0f9f2cfdda3daf4bc
Rodolfo D. Arce Sannemann <rodolfo.arce@wazuh.com>
2025-05-06 14:23:26 -0300

    Cleaning up for testing

--- wazuh_win_cmdlets/win_info.ps1
+++ wazuh_win_cmdlets/win_info.ps1
@@ -2,5 +2,5 @@ param(
     [switch]$Bios
     )
 if ($Bios) {
-    Get-WmiObject win32_bios | Select-Object -Property Name,SMBIOSBIOSVersion,Manufacturer,Version | ConvertTo-Json -Compress
+    Get-WmiObject win32_bios | Select-Object -Property Name,Manufacturer,Version,SerialNumber | ConvertTo-Json -Compress
 }
