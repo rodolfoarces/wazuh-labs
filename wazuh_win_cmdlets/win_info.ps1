7f155b728171ab8d5fff838acb325fd7ca278e23
Rodolfo D. Arce Sannemann <rodolfo.arce@wazuh.com>
2025-05-22 17:32:24 +0000

    Merging windows cmdlet branch

--- wazuh_win_cmdlets/win_info.ps1
+++ wazuh_win_cmdlets/win_info.ps1
@@ -1,1 +1,6 @@
-Get-Service -Name W32Time
+param(
+    [switch]$Bios
+    )
+if ($Bios) {
+    Get-WmiObject win32_bios | Select-Object -Property Name,Manufacturer,Version,SerialNumber | ConvertTo-Json -Compress
+}
