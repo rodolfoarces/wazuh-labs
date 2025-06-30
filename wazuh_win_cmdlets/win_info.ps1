9360fc3eb6b60d11533779abee18933f49173ab5
Rodolfo D. Arce Sannemann <rodolfo.arce@wazuh.com>
2025-06-30 18:07:53 -0300

    Structural changes to the code

--- wazuh_win_cmdlets/win_info.ps1
+++ wazuh_win_cmdlets/win_info.ps1
@@ -2,5 +2,6 @@ param(
     [switch]$Bios
     )
 if ($Bios) {
+    # Obtaing BIOS information and output JSON compressed format text
     Get-WmiObject win32_bios | Select-Object -Property Name,Manufacturer,Version,SerialNumber | ConvertTo-Json -Compress
 }
